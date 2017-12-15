d84d98036fa9a6365377f165aad0f32efd78252f
William <he.william@yahoo.com>
2017-12-14 16:08:55 -0800

    added routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import HomePage from './components/HomePage';
+import AboutMePage from './components/AboutMePage'
+import ProjectPage from './components/ProjectPage'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+        <div>
+            <Switch>
+                <Route path="/project" component={ProjectPage}/>
+                <Route path="/aboutme" component={AboutMePage}/>
+                <Route path="/" component={HomePage}/>
+            </Switch>
+        </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
