b6a44c9e340ac3f0b76536e98808e3a4bdd20ccb
William <he.william@yahoo.com>
2017-12-18 15:50:38 -0800

    added styling and new emblem and nav components

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import Emblem from './Emblem';
+import Nav from './Nav'
 
 export default class HomePage extends Component {
   render() {
     return (
-      <div>homepage</div>
+      <div>
+        <Emblem />
+        <Nav />
+        <div className="col-sm-6">
+          <div className="name">WILLIAM HE</div>
+            <div className="description">Full Stack Developer. 
+              <br/>Specializing in React.
+            </div>
+            <div className="description">
+              A knack for scaling or optimizing front end architecture and building reactive and performant UIs
+            </div>
+        </div>
+      </div>
     );
   }
 }
