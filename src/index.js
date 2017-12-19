e6fb9f524276703e4ae993356c983b6183963797
William <he.william@yahoo.com>
2017-12-18 18:38:00 -0800

    added muiTheme

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import reducers from './reducers';
 import HomePage from './components/HomePage';
@@ -12,15 +13,17 @@ import ProjectPage from './components/ProjectPage'
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-        <div>
-            <Switch>
-                <Route path="/project" component={ProjectPage}/>
-                <Route path="/aboutme" component={AboutMePage}/>
-                <Route path="/" component={HomePage}/>
-            </Switch>
-        </div>
-    </BrowserRouter>
-  </Provider>
-  , document.querySelector('.container'));
+    <MuiThemeProvider>
+        <Provider store={createStoreWithMiddleware(reducers)}>
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route path="/project" component={ProjectPage} />
+                        <Route path="/aboutme" component={AboutMePage} />
+                        <Route path="/" component={HomePage} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        </Provider>
+    </MuiThemeProvider>
+    , document.querySelector('.container'));
