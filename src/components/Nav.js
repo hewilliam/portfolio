c4e76db18aa52a9f14528ba24114cd88ecece0bc
William <he.william@yahoo.com>
2017-12-18 18:37:30 -0800

    src/components/Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import { Link } from 'react-router-dom';
+
 
 export default class Nav extends Component {
-    render () {
+    render() {
         return (
             <div>
-                Nav
+                <IconMenu
+                    iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                >
+                    <MenuItem primaryText="Home" containerElement={<Link to="/" />}/>
+                    <MenuItem primaryText="About Me" containerElement={<Link to="/aboutme" />}/>
+                    <MenuItem primaryText="Projects" containerElement={<Link to="/project" />}/>
+                </IconMenu>
             </div>
         )
     }
