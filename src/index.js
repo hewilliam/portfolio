18515a0424b7e08a99cd744cb9e3516589774111
William <he.william@yahoo.com>
2017-12-20 16:34:18 -0800

    added muiTheme Dark

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import reducers from './reducers';
 import HomePage from './components/HomePage';
@@ -13,7 +15,7 @@ import ProjectPage from './components/ProjectPage'
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
         <Provider store={createStoreWithMiddleware(reducers)}>
             <BrowserRouter>
                 <div>
